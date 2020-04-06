55df5415530b8a9930805f4dd06464809a6c5510
Timo Gremler <timo@timogremler.de>
2020-04-06 22:49:20 +0200

    add help with /? and /help parameter

--- windows_OS/create.bat
+++ windows_OS/create.bat
@@ -5,14 +5,34 @@ set flag=%2
 set visibility=%3
 cd /d %~dp0
 
-If "%1"=="" (
-    echo "error"
-) else ( 
-    if "%2"=="g" (
-        python remote.py %fn% %flag% %visibility%
-        ) else (
-            if "%2"=="l" (
-                python local.py %fn%
-            )
-        )
+
+
+If "%1"=="" if "%1" == "/?" if "%1" == "/help" (
+    goto:write_Help
+) else (
+    goto:write_Help
+) 
+
+
+if "%2"=="g" (
+    python remote.py %fn% %flag% %visibility%
+) else (
+    if "%2"=="l" (
+        python local.py %fn%
     )
+)
+
+
+:write_Help
+    echo.
+    echo Syntax: create ^<name^> ^<g/l^> ^<public/private^>
+    echo.
+    echo Optinons: 
+    echo    ^<name^>        Name of the project
+    echo    ^<g^>           GitHub repository 
+    echo    ^<l^>           Local repository
+    echo    ^<public^>      Public repository (only in GitHub repositories)
+    echo    ^<private^>     Private repository (only in GitHub repositories)
+    echo.
+EXIT /B 0
+
